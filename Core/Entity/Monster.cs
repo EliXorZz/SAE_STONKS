9c6287ca43aa14f53ccdf8bfdc976485278add85
clems <clems@SO6>
2023-01-10 00:14:01 +0100

    attaque des monstres corrigé

--- Core/Entity/Monster.cs
+++ Core/Entity/Monster.cs
@@ -11,25 +11,66 @@ namespace TheGame.Core
     {
         private MainGame _game;
         private float _speed;
-
+        bool _attack;
         private float _deadTime;
+        private DateTime _tempsAnimation;
+        private int _sens; 
 
         public Monster(MainGame game, string spriteName, Vector2 position, float speed, int health, int damage, int damageCooldown)
             : base(game, spriteName, position, 0.15f, health, damage, damageCooldown)
         {
             _game = game;
             _speed = speed;
-
+            _attack = false;
             _deadTime = 0;
         }
 
         public Monster(MainGame game, string spriteName, float speed, int health, int damage, int damageCooldown)
-            : this(game, spriteName, Vector2.Zero, speed, health, damage, damageCooldown) {}
+            : this(game, spriteName, Vector2.Zero, speed, health, damage, damageCooldown) { }
 
         public float Speed
         {
             get => _speed;
             set => _speed = value;
+        }
+
+        public bool Attack1
+        {
+            get
+            {
+                return _attack;
+            }
+
+            set
+            {
+                _attack = value;
+            }
+        }
+
+        public DateTime TempsAnimation
+        {
+            get
+            {
+                return _tempsAnimation;
+            }
+
+            set
+            {
+                _tempsAnimation = value;
+            }
+        }
+
+        public int Sens
+        {
+            get
+            {
+                return _sens;
+            }
+
+            set
+            {
+                _sens = value;
+            }
         }
 
         public override void Update(GameTime gameTime, Map map)
@@ -47,7 +88,7 @@ namespace TheGame.Core
                         target = player;
                     }
                 }
-                
+
                 if (target != null)
                 {
                     Rectangle monsterBounds = GetBounds();
@@ -56,17 +97,19 @@ namespace TheGame.Core
                     Vector2 targetCenter = target.GetCenter();
                     Vector2 center = GetCenter();
 
-                    if ((int)center.X != (int)targetCenter.X)
+                    if ((int)center.X != (int)targetCenter.X && !Attack1)
                     {
                         if (targetCenter.X < center.X)
                         {
                             Velocity.X = -Speed;
                             Animation = "run_left";
+                            Sens = -1;
                         }
                         else if (targetCenter.X > center.X)
                         {
                             Velocity.X = Speed;
                             Animation = "run_right";
+                            Sens = 1;
                         }
                     }
                     else
@@ -74,15 +117,36 @@ namespace TheGame.Core
                         Velocity.X = 0;
                         Animation = "idle";
                     }
-                    
+
                     if (monsterBounds.Intersects(targetBounds))
                     {
-                        if (Attack(target))
-                            if (center.X < targetCenter.X)
-                                Animation = "attack_right";
-                            else
-                                Animation = "attack_left";
+                        if (!this.Attack1)
+                        {
+                            this.Attack1 = true;
+                            this.TempsAnimation = DateTime.Now.AddMilliseconds(1400);
+
+                            
+
+                        }
+                        if (Sens == 1 && Attack1)
+                            Animation = "attack_right";
+                        else if(Attack1 && Sens == -1)
+                            Animation = "attack_left";
+
                     }
+                    if (Sens == 1 && Attack1)
+                        Animation = "attack_right";
+                    else if (Attack1 && Sens == -1)
+                        Animation = "attack_left";
+                    if (DateTime.Now >= this.TempsAnimation && Attack1)
+                    {
+                        this.Attack1 = false;
+                        if (monsterBounds.Intersects(targetBounds))
+                        {
+                            Attack(target);
+                        }
+                    }
+
                 }
 
                 if (IsCollisionMap(map, 0, 1))
@@ -97,7 +161,7 @@ namespace TheGame.Core
                 {
                     _deadTime += elapsed;
                     Animation = "death";
-                    
+
                     Sprite.Alpha = _deadTime / 800;
                 }
                 else
@@ -115,24 +179,44 @@ namespace TheGame.Core
             {
                 Rectangle bounds = GetBounds();
                 int barOffsetY = -4;
-            
+
                 ProgressBar healthBar =
                     new ProgressBar(_game, ScreenState.InGame, bounds.X, bounds.Y - barOffsetY - 8, bounds.Width,
-                        8, 1, (float) Health / MaxHealth, String.Empty,
+                        8, 1, (float)Health / MaxHealth, String.Empty,
                         Color.Transparent, Color.Transparent, new Color(46, 204, 113)
                     );
-            
-                ProgressBar cooldownBar =
+
+                /*ProgressBar cooldownBar =
                     new ProgressBar(_game, ScreenState.InGame, bounds.X, bounds.Y - barOffsetY, bounds.Width,
                         4, 1, -(CurrentDamageCooldown / DamageCooldown) + 1, String.Empty,
                         Color.Transparent, Color.Transparent, new Color(241, 196, 15)
-                    );
+                    );*/
 
                 healthBar.Draw(spriteBatch);
-                cooldownBar.Draw(spriteBatch);
+                //cooldownBar.Draw(spriteBatch);
             }
-            
+
             base.Draw(spriteBatch, globalUIBatch);
+
+        }
+        public void Attack(Player entity)
+        {
+
+            Random _random = new Random();
+            int realDamage = _random.Next(1, Damage);
+
+            entity.Health -= realDamage;
+
+
+
+            entity.AddFadeInterfaceComponent(
+                200,
+                1500,
+                new Vector2(0, -3),
+                new Text(_game, ScreenState.InGame, "font", 0, 0, $"-{realDamage}", Color.Red));
+
+
+
         }
     }
 }
